bf86e1d81973e2de837cdc560433b5562a112237
P4 <P4@DESKTOP-0BG23MU>
2021-09-30 12:40:40 -0400

    made it so you can't resize the window

--- CashRegister/Form1.Designer.cs
+++ CashRegister/Form1.Designer.cs
@@ -312,7 +312,9 @@ namespace CashRegister
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "The COVID Shop";
             this.Load += new System.EventHandler(this.Form1_Load);
